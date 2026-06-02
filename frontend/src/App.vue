138698fa09157d532fa2b1094530b0c148e3802d
Bianca Borba <biancaborba@outlook.com>
2026-06-01 22:55:04 -0300

    feat: criação de tela de cadastro e ajustes da navegação na introdução

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,18 @@
+
 <script setup>
   import { ref, computed } from "vue";
   import Introducao from "./Pages/Introducao.vue";
   import Login from "./Pages/Login.vue";
   import Home from "./Pages/Home.vue";
-  import PerfilMenu from "./Pages/Perfilmenu.vue";
-  import Estante from "./Pages/Estante.vue";
-  import MinhaLista from "./Pages/MinhaLista.vue";
+  import Cadastro from "./Pages/Cadastro.vue";
+
+  console.log(Cadastro);
 
   const routes = {
     "": Home,
     "login": Login,
     "introducao": Introducao,
-    "perfilMenu": PerfilMenu,
-    "estante": Estante,
-    "lista": MinhaLista,
+    "cadastro": Cadastro,
   }
 
   const currentRoute = ref("");
